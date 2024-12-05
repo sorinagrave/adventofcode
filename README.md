5b9da5dcefb1b588b261a1e89aebe263ef9405f1
Sorina Grave <sorina.grave@sky.uk>
2024-12-05 11:03:27 +0000

    completed day 1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # adventofcode
+
+## linting
+
+npx @biomejs/biome format --write
+
+## building
+
+tsc day1/HistorianHisteria.ts 
+
+## running 
+
+node day1/HistorianHisteria.js 
--- day1/HistorianHisteria.ts
+++ day1/HistorianHisteria.ts
@@ -4,10 +4,10 @@ const DATA_FILE = "day1/realInput.txt";
 const LINE_DELIMITER = "\n";
 const ARRAY_DELIMITER = "   ";
 
-const calculateDistances = () => {
-  const arrayLeft: number[] = [];
-  const arrayRight: number[] = [];
+const arrayLeft: number[] = [];
+const arrayRight: number[] = [];
 
+const loadArrays = () => {
   const arrayLines = fs
     .readFileSync(DATA_FILE)
     .toString()
@@ -20,6 +20,9 @@ const calculateDistances = () => {
       arrayRight.push(Number(right));
     }
   });
+};
+
+const calculateDistances = () => {
 
   const sortedLeft = arrayLeft.sort((n1, n2) => n1 - n2);
   const sortedRight = arrayRight.sort((n1, n2) => n1 - n2);
@@ -30,7 +33,25 @@ const calculateDistances = () => {
     0,
   );
 
+  console.log("DISTANCES:");
   console.log(sumOfDifferences);
 };
 
+const calculateSimilarities = () => {
+
+  const sims = arrayLeft.reduce(
+    (accumulator, currentValue) =>
+      accumulator +
+      currentValue *
+        arrayRight.filter((currentRight) => currentRight === currentValue)
+          .length,
+    0,
+  );
+
+  console.log("SIMILARITIES:");
+  console.log(sims);
+};
+
+loadArrays();
 calculateDistances();
+calculateSimilarities();
